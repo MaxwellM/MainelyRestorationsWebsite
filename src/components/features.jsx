4f4f12e142299e98057e30b3066b0e55ecad0403
MaxwellM <maxwellrmorin@gmail.com>
2021-09-22 22:31:07 -0600

    Mobile enhanced!

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -8,7 +8,7 @@ export const Features = (props) => {
         <div className='feature-row'>
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.title}-${i}`} className='col-xs-6 col-md-3'>
+                <div key={`${d.title}-${i}`} className='col-md-4'>
                   {' '}
                   <img src={d.img} alt=''  className='feature-img'/>{' '}
                   {/*<i className={d.img}></i>*/}
