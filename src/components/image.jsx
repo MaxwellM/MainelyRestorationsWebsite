7a5de3f1b92ca8e31890f7f5bf1267c96d0363b5
MaxwellM <maxwellrmorin@gmail.com>
2021-09-24 13:11:14 -0600

    New Lightbox!

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,23 +1,56 @@
-export const Image = ({ title, largeImage, smallImage }) => {
+import { SRLWrapper } from "simple-react-lightbox";
+
+const options = {
+    settings: {
+        autoplaySpeed: 0,
+        boxShadow: 'none',
+        disableKeyboardControls: true,
+        //disablePanzoom: false,
+        disableWheelControls: true,
+        //hideControlsAfter: 3000,
+        lightboxTransitionSpeed: 0.3,
+        lightboxTransitionTimingFunction: 'linear',
+        overlayColor: 'rgba(30, 30, 30, 0.9)',
+        slideAnimationType: 'fade',
+        slideSpringValues: [300, 50],
+        slideTransitionSpeed: 0.6,
+        slideTransitionTimingFunction: 'linear',
+        usingPreact: false,
+        showThumbnailsButton: false,
+    },
+    buttons: {
+        backgroundColor: 'rgba(30,30,36,0.8)',
+        iconColor: 'rgba(255, 255, 255, 0.8)',
+        iconPadding: '10px',
+        showAutoplayButton: false,
+        showCloseButton: true,
+        showDownloadButton: true,
+        showFullscreenButton: true,
+        showNextButton: false,
+        showPrevButton: false,
+        showThumbnailsButton: false,
+        size: '40px'
+    },
+    thumbnails: {
+        showThumbnails: false,
+    }
+}
+
+export const Image = ({ title, image }) => {
   return (
-    <div className='portfolio-item'>
-      <div className='hover-bg'>
-        {' '}
-        <a
-          href={largeImage}
-          title={title}
-          data-lightbox-gallery='gallery1'
-        >
-          <div className='hover-text'>
-            <h4>{title}</h4>
-          </div>
-          <img
-            src={smallImage}
-            className='img-responsive'
-            alt={title}
-          />{' '}
-        </a>{' '}
-      </div>
-    </div>
+      <SRLWrapper options={options}>
+        <div className='portfolio-item'>
+          {/*<div className='hover-bg'>*/}
+            {' '}
+            <div className='gallery-text'>
+                <h4>{title}</h4>
+            </div>
+            <a href={image} title={title} >
+              <img src={image} className='img-responsive' alt={title}
+              />{' '}
+            </a>{' '}
+          {/*</div>*/}
+        </div>
+      </SRLWrapper>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ReactLightbox from 'simple-react-lightbox'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <ReactLightbox>
+          <App />
+      </ReactLightbox>
   </React.StrictMode>,
   document.getElementById('root')
 );
